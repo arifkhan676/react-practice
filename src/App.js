b1875362866ae7ca97b97b4b24d45fed79079c0b
arifkhan676 <2014751049@uits.edu.bd>
2023-07-13 22:45:05 +0600

    delete ,condition

--- src/App.js
+++ src/App.js
@@ -8,38 +8,62 @@ class App extends Component {
 
   state = {
     book: [
-      { Bookname: "Rachel", Author: "Micel" },
-      { Bookname: "Paradoxical", Author: "Ariof Azad" }
-    ]
+      { id: 1, Bookname: "Rachel", Author: "Micel" },
+      { id: 2, Bookname: "Paradoxical", Author: "Ariof Azad" },
+      { id: 3, Bookname: "Software", Author: "Ahmed Shamim" }
+    ],
+    showBooks: true
   }
 
-  onChange = (bookname) => {
+  changeInput = (e, index) => {
+    const bookSet = {
+      ...this.state.book[index]
+    }
+    bookSet.Bookname = e.target.value;
+    const book = [...this.state.book]
+    book[index] = bookSet;
+    this.setState({ book: book })
+  }
+
+  deleteChange = index => {
+    const deleteBooks = this.state.book;
+    deleteBooks.splice(index, 1);
     this.setState({
-      book: [
-        { Bookname: "Software", Author: "Ahmed Shamim" },
-        { Bookname: "X Company", Author: "Arif Bro" },
-        { Bookname: "y Company", Author: " Bro" }
-      ],
-      otherProp: "I am some other props"
+      book: deleteBooks
     })
   }
+
+  toggleChange = () => {
+    this.setState({ showBooks: !this.state.showBooks })
+  }
+
 
   render() {
 
     const BookState = this.state.book;
-    const BookList = BookState.map((item) => {
-      return (
-        <Book Bookname={item.Bookname} Author={item.Author} />
-      );
-    });
-    console.log(BookList);
+    let book = null;
+    if (this.state.showBooks) {
+      book = BookState.map((item, index) => {
+        return (
+          <Book
+            Bookname={item.Bookname}
+            Author={item.Author}
+            delete={() => this.deleteChange(index)}
+            key={item.id}
+            inputName={(e) => this.changeInput(e, index)}
+          />
+        );
+      });
+    }
+
 
 
     return (
       <div className="App">
         <h1>Hello Arif</h1>
-        <button onClick={() => this.onChange("Rich Dad Poor Dad")}  > Click to change </button>
-        {BookList}
+        <button onClick={this.toggleChange} > toggle to booklist </button>
+
+        {book}
       </div>
     );
   }
--- src/Book.jsx
+++ src/Book.jsx
@@ -1,35 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 const Book = (props) => {
     const date = new Date().getFullYear();
 
-    const [inp, fullInp] = useState();
-    const [getInp, FullgetInp] = useState([]);
-
-
-
-    function changeInput(e) {
-        const name = e.target.value;
-        fullInp(name);
-    }
-
-    function handleclick(e) {
-        FullgetInp(inp);
-    }
-
     return (
         <div className="App">
+            <button style={{ color: "red" }} onClick={props.delete} > Delete to the booklist </button>
             <h1> Bookname: {props.Bookname}  </h1>
             <h1> Author : {props.Author} </h1>
             <p>  Published in : {date} </p>
-            <button style={{ color: "red" }} onClick={props.Name} > Change  </button>
-            <input name="title" type="text" onChange={changeInput} />
-            <button onClick={handleclick} >handleClick</button>
-            <p> {inp} </p>
-            <h3>{getInp}</h3>
+            <input type="text" onChange={props.inputName} />
+            {/* <button onClick={handleclick} >change Book Name</button> */}
+
         </div>
     );
 }
 
-export default Book; 
+export default Book;
+
