4c80ae1b1eb6b9ed9bd1ef1ddc010f90a8b556c9
arifkhan676 <2014751049@uits.edu.bd>
2023-07-16 18:21:59 +0600

    clickk

--- src/components/lists/NewBook.js
+++ src/components/lists/NewBook.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const NewBook = ({ selectBook }) => {
+    const a = 3 + 4;
     return selectBook && (
         <div>
             <h3> {selectBook.Bookname} </h3>
             <h3> {selectBook.Author} </h3>
+            <p>{a}</p>
         </div>
     )
 }
 
 export default NewBook
+
+
+//some changes
